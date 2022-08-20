9dfc803979f447e1ae1432695e0d610662447ba9
MhnnXdev <muhafandeveloper@gmail.com>
2022-08-20 14:41:03 +0700

    Update 1

--- Latihan_DB/Form_Login.Designer.cs
+++ Latihan_DB/Form_Login.Designer.cs
@@ -40,7 +40,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(123, 38);
+            this.label1.Location = new System.Drawing.Point(136, 87);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(112, 22);
             this.label1.TabIndex = 0;
@@ -48,7 +48,7 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(165, 147);
+            this.txtPassword.Location = new System.Drawing.Point(166, 188);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(161, 20);
             this.txtPassword.TabIndex = 1;
@@ -56,9 +56,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(142, 201);
+            this.button1.Location = new System.Drawing.Point(147, 257);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(90, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,7 +66,7 @@
             // 
             // txtNama
             // 
-            this.txtNama.Location = new System.Drawing.Point(165, 112);
+            this.txtNama.Location = new System.Drawing.Point(166, 153);
             this.txtNama.Name = "txtNama";
             this.txtNama.Size = new System.Drawing.Size(161, 20);
             this.txtNama.TabIndex = 1;
@@ -75,7 +75,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(57, 111);
+            this.label2.Location = new System.Drawing.Point(58, 154);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 18);
             this.label2.TabIndex = 0;
@@ -85,7 +85,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(57, 146);
+            this.label3.Location = new System.Drawing.Point(58, 189);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 18);
             this.label3.TabIndex = 0;
